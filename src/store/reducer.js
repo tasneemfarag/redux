50374de7fcc3b35901adacc02c771623c30f65e5
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-11-04 11:59:09 -0500

    switch-case in the reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,25 +3,23 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
-    }
-    if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if (action.type === 'ADD') {
-        return {
-            counter: state.counter + action.payload
-        }
-    }
-    if (action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - action.payload
-        }
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + action.payload
+            }
+        case 'SUBTRACT':
+            return {
+                counter: state.counter - action.payload
+            }
     }
     return state;
 };
